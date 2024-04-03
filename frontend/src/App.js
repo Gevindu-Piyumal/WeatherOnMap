3dc6591ae58b7f3338ff503f9931696574648b22
Gevindu Piyumal <gevindu.personal@gmail.com>
2024-04-04 01:57:07 +0530

    tooltip added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import {
   MapContainer,
   TileLayer,
-  Polygon
+  Polygon,
+  Tooltip
 } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { statesData } from './data';
@@ -17,52 +18,54 @@ export default function App() {
       zoom={8}
       style={{ width: '100vw', height: '100vh' }}
     >
+
       <TileLayer
-        url="https://api.maptiler.com/maps/landscape/256/{z}/{x}/{y}.png?key=TRf2ueSa5fdmCgl7S2Pm"
+        url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=TRf2ueSa5fdmCgl7S2Pm"
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
       />
       {
-      statesData.features.map((state)=> {
-        const coordinates = state.geometry.coordinates[0].map((item) => [item[1], item[0]]);
-
-        return (<Polygon
-          pathOptions={{
-            fillColor: '#FD8D3C',
-            fillOpacity: 0.7,
-            weight: 2,
-            opacity: 1,
-            dashArray: 3,
-            color: 'white'
-          }}
-          positions={coordinates}
-          eventHandlers={{
-            mouseover: (e) => {
-              const layer = e.target;
-              layer.setStyle({
-                dashArray: "",
-                fillColor: "#BD0026",
-                fillOpacity: 0.7,
-                weight: 2,
-                opacity: 1,
-                color: "white",
-              })
-            },
-            mouseout: (e) => {
-              const layer = e.target;
-              layer.setStyle({
-                fillOpacity: 0.7,
-                weight: 2,
-                dashArray: "3",
-                color: 'white',
-                fillColor: '#FD8D3C'
-              });
-            },
-            click: (e) => {
-              window.alert(state.properties.name + " " + state.properties.density)
-            }
-          }}
-        />)
-      })
+        statesData.features.map((state) => {
+          const coordinates = state.geometry.coordinates[0].map((item) => [item[1], item[0]]);
+          return (<Polygon
+            pathOptions={{
+              fillColor: 'green',
+              fillOpacity: 0.1,
+              weight: 2,
+              opacity: 1,
+              dashArray: 3,
+              color: 'white'
+            }}
+            positions={coordinates}
+            eventHandlers={{
+              mouseover: (e) => {
+                const layer = e.target;
+                layer.setStyle({
+                  dashArray: "",
+                  fillColor: "green",
+                  fillOpacity: 0.3,
+                  weight: 2,
+                  opacity: 1,
+                  color: "white",
+                })
+              },
+              mouseout: (e) => {
+                const layer = e.target;
+                layer.setStyle({
+                  fillOpacity: 0.1,
+                  weight: 2,
+                  dashArray: "3",
+                  color: 'white',
+                  fillColor: 'green'
+                });
+              },
+              click: (e) => {
+                window.alert(state.properties.name + " " + state.properties.density)
+              }
+            }}
+          >
+            <Tooltip>{state.properties.name}</Tooltip>
+          </Polygon>)
+        })
       }
     </MapContainer>
   );
